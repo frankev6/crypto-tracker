1f7afdc9f0649a9c5515a3ac600a84791496e7ad
frankev6 <frank.ev6@gmail.com>
2022-05-15 06:36:26 -0400

    Added settings for additional customization

--- src/tracker.js
+++ src/tracker.js
@@ -1,13 +1,20 @@
+let userCoins;
+let currency;
+let settingsOpen = false;
+let updateTimeout;
+
 const track = async (coins) => {
   return await fetch(
-    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&ids=${coins.join(
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coins.join(
       ","
     )}`
   );
 };
 
 const drawCoin = (coin) => {
-  return `<a target="_blank" href="https://www.coingecko.com/en/coins/${
+  return `<a id='${
+    coin.name
+  }' target="_blank" href="https://www.coingecko.com/en/coins/${
     coin.id
   }"><li class="coin-item"><div><p class="coin-rank">${
     coin.market_cap_rank
@@ -15,13 +22,17 @@ const drawCoin = (coin) => {
     coin.name
   }</p><p class="coin-symbol">${coin.symbol.toUpperCase()}</p></div><div><p class='fade-in'>${
     coin.current_price
-  }</p><p class="fade-in coin-percent ${
+  } ${currency}</p><p class="fade-in coin-percent ${
     coin.price_change_percentage_24h > 0 ? "positive" : "negative"
-  }">${coin.price_change_percentage_24h.toFixed(2)}%</p></div></li></a>`;
+  }">${coin.price_change_percentage_24h.toFixed(2)}%</p>
+  </div></li></a>`;
 };
 
-const update = () => {
-  track(["algorand", "terra-luna", "ethereum", "bitcoin"]).then((res) =>
+const update = (clearTimeout = false) => {
+  if (clearTimeout) {
+    window.clearTimeout(updateTimeout);
+  }
+  track(userCoins).then((res) =>
     res.json().then((r) => {
       console.log(r);
 
@@ -35,7 +46,75 @@ const update = () => {
     })
   );
 
-  setTimeout(update, 10000);
+  updateTimeout = setTimeout(update, 10000);
 };
 
-update();
+const setUserCurrency = (newCurrency) => {
+  localStorage.setItem("currency", newCurrency);
+  currency = newCurrency;
+  update(true);
+};
+
+const setUserCoins = () => {
+  localStorage.setItem(
+    "userCoins",
+    document.getElementById("user-coins-input").value.toLowerCase()
+  );
+  userCoins = document
+    .getElementById("user-coins-input")
+    .value.toLowerCase()
+    .split(",");
+
+  update(true);
+};
+
+const addUserCoin = () => {};
+
+const removeUserCoin = () => {};
+
+const toggleSettings = () => {
+  settingsOpen = !settingsOpen;
+
+  document.getElementById("settings-form").style.display = !settingsOpen
+    ? "none"
+    : "block";
+  document.getElementById("coin-list-container").style.display = settingsOpen
+    ? "none"
+    : "block";
+};
+
+const initTracker = () => {
+  userCoins = localStorage.getItem("userCoins").split(",");
+  currency = localStorage.getItem("currency");
+
+  document.getElementById("user-coins-input").value = userCoins;
+
+  document.getElementById("settings-save").addEventListener("click", () => {
+    setUserCoins();
+    toggleSettings();
+  });
+
+  if (userCoins && userCoins.length > 0) {
+    update();
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  initTracker();
+
+  document
+    .getElementById("user-coins-input")
+    .addEventListener("keypress", (e) => {
+      if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+        setUserCoins();
+        toggleSettings();
+      }
+    });
+
+  document.getElementById("btn-settings").onclick = toggleSettings;
+  document.getElementById("currency-selection").onchange = () => {
+    setUserCurrency(document.getElementById("currency-selection").value);
+  };
+  document.getElementById("currency-selection").value = currency;
+});
